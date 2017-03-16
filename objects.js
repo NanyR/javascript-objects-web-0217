5163c2c049e5d182b7258553597bf0432a1cbada
nanyr <nanyramirez0@gmail.com>
2017-03-16 18:23:57 -0400

    Done.

---
+++ objects.js
@@ -0,0 +1,13 @@
+var playlist={
+  XX: "angels"
+}
+
+function updatePlaylist(playlist, artistName, songTitle){
+  playlist[artistName]= songTitle;
+  return playlist
+}
+
+function removeFromPlaylist(playlist, artistName){
+  delete playlist[artistName];
+  return playlist
+}
